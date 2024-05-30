dbb6477089d372ef8372fff388dedd3b46c49e1c
Gustavo Prado <73006886+devgustta@users.noreply.github.com>
2024-05-30 12:37:46 -0300

    Update organizando.py

--- organizando.py
+++ organizando.py
@@ -9,7 +9,7 @@ def criandoDiretorios(caminho, diretorio):
     return 0
 
 def verificaDir():
-    caminho = "C:\\Users\\gsp37\\OneDrive\\Área de Trabalho\\teste"
+    caminho = "C:\\Users\\**\\**\\**\\teste"
     for root, _, files in os.walk(caminho):
         for file in files:
             filename, file_extension = os.path.splitext(f'{root}\\{file}')
